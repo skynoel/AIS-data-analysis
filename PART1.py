1a81e71fe4ca0536406db370112613b6418e1679
skynoel <66463016+skynoel@users.noreply.github.com>
2024-01-10 03:34:45 +0800

    v0.0.2

--- PART1.py
+++ PART1.py
@@ -12,12 +12,15 @@ pd.set_option('display.width', 180)
 
 df=pd.read_excel("Decodered LOG (2023-06-03) 001.xlsx")
 
-
+#整理資料，計算每分鐘封包數
 df2=df.pivot_table(index=["RMC_TAKEN_AT_HOUR","RMC_TAKEN_AT_MIN"],values=["PACKET_TYPE"],aggfunc={"RMC_TAKEN_AT_HOUR":np.size})
 df2 = df2.rename(columns={'RMC_TAKEN_AT_HOUR': 'times'})
+#計算小時中每分鐘平均封包數
 df3=df2.groupby('RMC_TAKEN_AT_HOUR').mean()
+#匯出整理後資料
 df3.to_excel("Decodered LOG (2023-06-03) 001.xlsx"+"data.xlsx")
 
+#引入整理後資料，並生成統計圖
 data=openpyxl.load_workbook("Decodered LOG (2023-06-03) 001.xlsx"+"data.xlsx")
 sheet=data.worksheets[0]
 sheet.delete_rows(idx=1)
